be4c25735c3d3219f21c290f56719e28654cab3f
cotiaws <cotiaws5@gmail.com>
2025-10-17 08:53:09 -0300

    Camadas de repositório e serviços de domínio.

---
+++ FinancasApp.Domain/Mappings/ProfileMap.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using FinancasApp.Domain.Dtos.Requests;
+using FinancasApp.Domain.Dtos.Responses;
+using FinancasApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancasApp.Domain.Mappings
+{
+    /// <summary>
+    /// Configuração dos mapeamentos do AutoMapper
+    /// </summary>
+    public class ProfileMap : Profile
+    {
+        public ProfileMap()
+        {
+            CreateMap<CategoriaRequest, Categoria>();
+            CreateMap<MovimentacaoRequest, Movimentacao>();
+
+            CreateMap<Categoria, CategoriaResponse>();
+            CreateMap<Movimentacao, MovimentacaoResponse>();
+        }
+    }
+}
